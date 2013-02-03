51158583a9dcf55555e421428fed68bec2090adb
iMouad <Mouad.Softs@gmail.com>
2013-02-03 08:19:26 -0800

    CheckSpace
    CheckSpace Function to check space in names .

---
+++ Checkspace.php
@@ -0,0 +1,24 @@
+<?php
+##########################################################
+# @name : Function Checkspace                            # 
+# @version : Version 1.0                                 #
+# @author : Mouad Dabdoubi                               #
+# @copyrights : 2013 ©                                   #
+# @license : LGPL <http://www.gnu.org/licenses/lgpl.txt> #
+##########################################################
+ 
+function Checkspace ($val){ 
+
+    $reg= '@^[A-Za-z_-أ-ي-]+$@'; 
+  
+    if (preg_match($reg,$val)){ 
+    echo "<b>الاسم صحيح</b>"; 
+    }else{ 
+        echo "<b>! الاسم يحتوي على فراغات</b>"; 
+    }
+} 
+  
+
+$var = "معاذ mouad"; 
+echo Checkspace($var); 
+?>
